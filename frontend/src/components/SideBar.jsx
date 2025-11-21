bc2ba9419a60f7e7afffea66e9ee2911441c4123
Jitesh-Gadage16 <gadagejitesh16@gmail.com>
2025-11-21 00:41:46 -0500

    changes

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -34,8 +34,7 @@ const SideBar = () => {
         {link("/createGroup", PlusCircleIcon, "Create group")}
         {link("/family-groups", UsersIcon, "My groups")}
         {link("/addMember", UserPlusIcon, "Add members")}
-        {link("/caregiver", UserPlusIcon, "CareGiver")}
-        {link("/carerecipients", UserPlusIcon, "careRecipients")}
+
 
         {insideGroup && (
           <NavLink
