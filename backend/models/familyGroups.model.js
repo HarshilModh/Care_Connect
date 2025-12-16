340381d7f98f5170075ddd75e2befe1007645803
Modh Harshil Chetankumar <78226025+HarshilModh@users.noreply.github.com>
2025-12-15 23:50:44 -0500

    Enhance validation for family group names and descriptions; update routing for private access to chat, edit group, and notifications components.

--- backend/models/familyGroups.model.js
+++ backend/models/familyGroups.model.js
@@ -8,9 +8,14 @@ const familyGroupSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
       minlength: 2,
-      maxlength: 100
+      maxlength: 100,
+      //Group name must be valid like "Family Group 1 only numer are not allowed valid names: John Doe, FamilyGroup123 not valid names: 12345, @Family!"
+      match: [/^[a-zA-Z][a-zA-Z0-9\s]{1,99}$/, 'Group name must be valid can contain letters, numbers, and spaces, but not start with a number or special character.'],
     },
-    description: { type: String, trim: true, maxlength: 500, default: "" ,required:true},
+    description: { type: String, trim: true, maxlength: 500, default: "" ,required:true,
+      //Description can contain letters, numbers, spaces, and basic punctuation. but not special characters like <, >, {, },and also cannot contain only numbers
+      match: [/^(?!^\d+$)[a-zA-Z0-9\s.,'"\-!?()]{1,500}$/, 'Description can contain letters, numbers, spaces, and basic punctuation, but not special characters like <, >, {, }, and cannot contain only numbers.'],
+    },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
     timeZone: {
       type: String,
